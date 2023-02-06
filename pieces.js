02796d2f8263fc6a58d25e14b4e5b042b9eba426
psychoniac <jlnko@hotmail.fr>
2023-02-06 09:38:29 +0100

    creation des balises contenant les propriétés des produits

--- pieces.js
+++ pieces.js
@@ -1,1 +1,16 @@
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch('pieces-autos.json');
+const pieces = await reponse.json();
+
+//creation de la variable article qui contient le contenu du json 
 const article = pieces[0];
+//creation des variables contenant les balises html
+const nomElement = document.createElement("h2");
+nomElement.innerText = article.nom;
+const imageElement = document.createElement("img");
+imageElement.src = article.image;
+const prixElement = document.createElement("p");
+prixElement.innerText = 'Prix : ${article.prix} euro';
+const categorieElement = document.createElement("p");
+categorieElement.innerText = article.categorie;
+
